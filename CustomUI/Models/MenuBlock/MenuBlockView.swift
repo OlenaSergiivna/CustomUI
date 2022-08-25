584bda21dc6602e46bc0d2191ef62ae0d2482deb
Olena <elenasergeevvnna@gmail.com>
2022-08-25 21:42:32 +0300

    overrided inits

--- CustomUI/Models/MenuBlock/MenuBlockView.swift
+++ CustomUI/Models/MenuBlock/MenuBlockView.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class MenuBlockView: UIView {
 
-    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var contentView: UIView!
     
-}
+    
+    
+        //let kCONTENT_XIB_NAME = "PersonView"
+    
+        //@IBOutlet var lblFirstName: UILabel!
+       // @IBOutlet var lblLastName: UILabel!
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            commonInit()
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            commonInit()
+        }
+        
+        func commonInit() {
+            Bundle.main.loadNibNamed("CustomView", owner: self, options: nil)
+            contentView.fixInView(self)
+        }
+    }
