2ac475b744e6e8e42d967b55a38f43da340895fb
Olena <elenasergeevvnna@gmail.com>
2022-08-25 23:03:03 +0300

    added first menu block

--- CustomUI/Controllers/ViewController.swift
+++ CustomUI/Controllers/ViewController.swift
@@ -12,7 +12,9 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let menuBlock = MenuBlockView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        let menuBlock = MenuBlockView(frame: CGRect(x: view.frame.width / 8, y: view.frame.height / 8, width: 250, height: 250))
+        menuBlock.contentView.layer.cornerRadius = 40
+        
         self.view.addSubview(menuBlock)
     }
 
