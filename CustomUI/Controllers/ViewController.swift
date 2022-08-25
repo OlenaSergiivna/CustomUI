6dcb701d9deaeb07580adc4eff82b6818a315fb9
Olena <elenasergeevvnna@gmail.com>
2022-08-25 23:13:38 +0300

    finished first block frame settings

--- CustomUI/Controllers/ViewController.swift
+++ CustomUI/Controllers/ViewController.swift
@@ -11,12 +11,11 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let menuBlock = MenuBlockView(frame: CGRect(x: view.frame.minX + 20, y: view.frame.height / 8, width: 150, height: 150))
-        menuBlock.contentView.layer.cornerRadius = 40
-        
         self.view.backgroundColor = #colorLiteral(red: 0.1176470444, green: 0.1176470444, blue: 0.1176470444, alpha: 1)
         
+        let menuBlock = MenuBlockView(frame: CGRect(x: view.frame.minX + 20, y: view.frame.midY + 100, width: 200, height: 200))
+        menuBlock.contentView.layer.cornerRadius = 40
+       
         self.view.addSubview(menuBlock)
     }
 
