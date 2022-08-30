c554d0bf3e237157a9b9d2a33b32b452a0e03e55
Olena <elenasergeevvnna@gmail.com>
2022-08-31 00:14:07 +0300

    Small setting was made.

--- CustomUI/Controllers/MainViewController.swift
+++ CustomUI/Controllers/MainViewController.swift
@@ -38,24 +38,27 @@ class MainViewController: UIViewController, MenuBlockDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // MARK: Setting configurations to the Main View
         
         self.view.backgroundColor = #colorLiteral(red: 0.1176470444, green: 0.1176470444, blue: 0.1176470444, alpha: 1)
         
         
-        // MARK: Setting constraints programmatically to MainView's subviews
+        
+        // MARK: Setting frames programmatically to MainView's subviews
         
         let blockHeight = (view.frame.width - 45) / 2
         
         greenView.frame = CGRect(x: view.frame.minX, y: view.frame.midY - blockHeight * 1.2, width: view.frame.width, height: blockHeight)
         
-        limitLabel.fixInView(greenView, topConstant: blockHeight / 15, isActiveBottom: false, leadingConstant: 80, trailingConstant: -80)
+        limitLabel.frame = CGRect(x: greenView.frame.minX + 80, y: greenView.frame.height / 29, width: greenView.frame.width - 160, height: 30)
         
-        amountLabel.fixInView(greenView, topConstant: blockHeight / 4, isActiveBottom: false, leadingConstant: 140, trailingConstant: -140)
-        
-        
+        amountLabel.frame = CGRect(x: greenView.frame.minX + 140, y: greenView.frame.height / 4, width: greenView.frame.width - 285, height: 50)
+
         leftButton.frame = CGRect(x: greenView.frame.minX + 10, y: greenView.frame.height / 15, width: 25, height: 25)
+        
         rightButton.frame = CGRect(x: greenView.frame.width - 40, y: greenView.frame.height / 15, width: 25, height: 25)
+        
         
         
         // MARK: Adding and configuring three MenuBlocks programmatically
@@ -72,14 +75,14 @@ class MainViewController: UIViewController, MenuBlockDelegate {
         thirdMenuBlock.addAndConfigureMenuBlock(view: view, text: "Рассрочка на карту", x: view.frame.minX + 15.0, y: view.frame.midY - (blockHeight / 2) - 15, width: view.frame.maxX - 30.0, height: blockHeight, color: .systemGreen, image: "diagram")
         
         
-        // MARK: Setting MainViewController as delegate
+        // MARK: Setting MainViewController as delegate for three MenuBlocks
         
         firstMenuBlock.delegate = self
         secondMenuBlock.delegate = self
         thirdMenuBlock.delegate = self
         
         
-        // MARK: Adding CurrencyBlock programmatically
+        // MARK: Adding and configuring CurrencyBlock programmatically
         
         addCurrencyBlock(view: view, x: 0, y: view.frame.midY - blockHeight * 2, width: view.frame.maxX, height: blockHeight * 0.8)
         
--- CustomUI/Models/Functions/FontSizeFunction.swift
+++ CustomUI/Models/Functions/FontSizeFunction.swift
@@ -8,6 +8,8 @@
 import Foundation
 import UIKit
 
+// MARK: Function, that changes font depending on device type
+
 func deviceFontSizeSelector(label: UIButton, iPhone_8_SE: CGFloat = 17, iPod_touch: CGFloat = 13, iPhone_12_13_mini: CGFloat = 17, allDevices: CGFloat = 21) {
     let deviceType = UIDevice.current.deviceType
     
